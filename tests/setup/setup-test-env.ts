cc89eae7f8593f883ec2824df80eef0614ca3523
aydrian <aydrian@gmail.com>
2024-09-25 11:26:39 -0400

    chore: set vitest to run cleanup after each test

--- tests/setup/setup-test-env.ts
+++ tests/setup/setup-test-env.ts
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom/vitest'
 import { installGlobals } from '@remix-run/node'
+import { cleanup } from '@testing-library/react'
 import { afterAll, afterEach } from 'vitest'
 
 installGlobals()
@@ -7,5 +8,7 @@ installGlobals()
 // one time setup here
 // fs.copyFileSync(BASE_DATABASE_PATH, DATABASE_PATH)
 
-afterEach(() => {})
+afterEach(() => {
+  cleanup()
+})
 afterAll(async () => {})
